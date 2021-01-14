3790f7428ec6ee3463a95701fd7a98e399d13826
Eashwar Mohan <eashwar@umich.edu>
2021-01-14 14:23:18 -0500

    elif

--- bot.py
+++ bot.py
@@ -34,30 +34,30 @@ async def on_message(message, pass_context=True):
         contents = message.content.lower().split()
         if len(contents) != 2:
             await message.channel.send(f'<@{message.author.id}> error: unable to parse command. please use `help` for a list of valid commands.')
-        if contents[1] == 'help':
+        elif contents[1] == 'help':
             await message.channel.send('Here\'s a list of commands:\n'
                                       + '- `join` will let you join the office hours queue.\n' 
                                       + '- `status` will let you check your position in the queue.\n'
                                       + '- `list` will list all the students in the queue.\n'
                                       + f'- `pop` can only be used by instructors. it will remove the next student from the queue, and if they\'re in the {VOICE_WAITING_ROOM} channel, they\'ll be moved into the {VOICE_HELP_ROOM} channel.\n'
                                       + '- `clear` can only be used by instructors. it clears the queue completely.')
-        if contents[1] == 'ping':
+        elif contents[1] == 'ping':
             await message.channel.send('pong!')
-        if contents[1] == 'join':
+        elif contents[1] == 'join':
             if message.author in _queue:
                 await message.channel.send(f'<@{message.author.id}> you\'re already in line, position {_queue.index(message.author) + 1}. If you stay in the {VOICE_WAITING_ROOM} voice call, you will be moved into the {VOICE_HELP_ROOM} channel when it is your turn!')
             else:
                 _queue.append(message.author)
                 await message.channel.send(f'<@{message.author.id}> you\'ve been added to the queue, position {_queue.index(message.author) + 1}! If you stay in the {VOICE_WAITING_ROOM} voice call, you will be moved into the {VOICE_HELP_ROOM} channel when it is your turn!')
-        if contents[1] == 'status':
+        elif contents[1] == 'status':
             if message.author in _queue:
                 await message.channel.send(f'<@{message.author.id}>, you\'re  position {_queue.index(message.author) + 1} in the queue!')
             else:
                 await message.channel.send(f'<@{message.author.id}>, you\'re not in the queue. use `join` to join the queue.')
-        if contents[1] == 'list':
+        elif contents[1] == 'list':
             response = await get_list()
             await message.channel.send(response)
-        if contents[1] == 'pop':
+        elif contents[1] == 'pop':
             if any(role.name == ADMIN_ROLE_NAME for role in message.author.roles):
                 next_student = _queue.pop(0)
                 if next_student.voice != None and next_student.voice.channel.name == VOICE_WAITING_ROOM:
@@ -67,7 +67,7 @@ async def on_message(message, pass_context=True):
                     await message.channel.send(f'<@{next_student.id}>, it\'s your turn! Please join the {VOICE_WAITING_ROOM} voice call so that the instructor can pull you into the private call.')
             else:
                 await message.channel.send(f'<@{message.author.id}> error: you do not have permission to use this command.')
-        if contents[1] == 'clear':
+        elif contents[1] == 'clear':
             if any(role.name == ADMIN_ROLE_NAME for role in message.author.roles):
                 _queue.clear()
                 await message.channel.send('The office hours queue has been completely cleared.')
